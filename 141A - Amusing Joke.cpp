6f269513de4e1b178ddb744f7cfc7634089d33b1
dhruv-1108 <dhruv1108aggarwal@gmail.com>
2022-09-23 15:31:54 +0530

    Amusing Joke

--- 141A - Amusing Joke.cpp
+++ 141A - Amusing Joke.cpp
@@ -17,32 +17,26 @@ typedef unsigned long long int uint64;
 
 int main()
 {
-	ll tc;
-	cin >> tc;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-	while(tc--)
-	{
-		string str, s1 = "",s2 = "";
-		cin >> str;
+	string s1, s2, s3;
+	cin >> s1 >> s2 >> s3;
 
-		for(int i =0; i < (str.size()/2); i++)
-		{
-			s1 += str[i];
-		}
+	unordered_map<char, int> mp;
+	unordered_map<char, int> mpp;
 
-		for(int i = str.size()/2 ; i < str.size(); i++)
-		{
-			s2 += str[i];
-		}
+	for(auto x : s1)
+		mp[x]++;
+	for(auto x : s2)
+		mp[x]++;
 
-		// cout << s1 << endl;
-		// cout << s2 << endl;
+	for(auto i : s3)
+		mpp[i]++;
 
-		if(s1 == s2)
-			cout << "YES\n";
-		else
-			cout << "NO\n";
-	}
-
+	if(mp == mpp)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
 	return 0;
 }
